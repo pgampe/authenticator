93985b3169e2298d68d2fd2191ae8b18dd00d1aa
Philipp Gampe <philipp.gampe@typo3.org>
2013-05-03 11:43:33 +0200

    [TASK] Move phpqrcode into Resources/Private/Php

--- Resources/Private/Php/phpqrcode/qrlib.php
+++ Resources/Private/Php/phpqrcode/qrlib.php
@@ -29,15 +29,15 @@
 	
 	// Required libs
 	
-	include $QR_BASEDIR."qrconst.php";
-	include $QR_BASEDIR."qrconfig.php";
-	include $QR_BASEDIR."qrtools.php";
-	include $QR_BASEDIR."qrspec.php";
-	include $QR_BASEDIR."qrimage.php";
-	include $QR_BASEDIR."qrinput.php";
-	include $QR_BASEDIR."qrbitstream.php";
-	include $QR_BASEDIR."qrsplit.php";
-	include $QR_BASEDIR."qrrscode.php";
-	include $QR_BASEDIR."qrmask.php";
-	include $QR_BASEDIR."qrencode.php";
+	include $QR_BASEDIR . "qrconst.php";
+	include $QR_BASEDIR . "qrconfig.php";
+	include $QR_BASEDIR . "qrtools.php";
+	include $QR_BASEDIR . "qrspec.php";
+	include $QR_BASEDIR . "qrimage.php";
+	include $QR_BASEDIR . "qrinput.php";
+	include $QR_BASEDIR . "qrbitstream.php";
+	include $QR_BASEDIR . "qrsplit.php";
+	include $QR_BASEDIR . "qrrscode.php";
+	include $QR_BASEDIR . "qrmask.php";
+	include $QR_BASEDIR . "qrencode.php";
 
