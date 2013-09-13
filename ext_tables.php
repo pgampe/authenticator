25d4a9882def0e1e3f2f65d245bbbe618438e595
Philipp Gampe <philipp.gampe@typo3.org>
2013-09-13 20:19:52 +0200

    [FEATURE] Make QR code display in user records optional

--- ext_tables.php
+++ ext_tables.php
@@ -2,54 +2,77 @@
 if (!defined('TYPO3_MODE')) {
 	die ('Access denied.');
 }
-$tempColumns = array(
-	'tx_authenticator_secret' => array(
-		'exclude' => 0,
-		'label' => 'LLL:EXT:authenticator/locallang_db.xlf:be_users.tx_authenticator_secret',
-		'config' => array(
-			'type' => 'user',
-			'userFunc' => 'EXT:authenticator/Classes/Fields/QrFields.php:Tx\Authenticator\Fields\QrFields->getField'
-		)
-	),
-	'tx_authenticator_enabled' => array(
-		'label' => 'LLL:EXT:authenticator/locallang_db.xlf:be_users.tx_authenticator_enabled',
-		'config' => array(
-			'type' => 'check',
-			'items' => array(
-				array('LLL:EXT:authenticator/locallang_db.xlf:be_users.tx_authenticator_enabled_item', 0)
-			),
-			'default' => 0
-		)
-	),
+// Get EM settings
+$settings = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['authenticator']);
+if (!is_array($settings)) {
+    $settings = array();
+}
+
+/*****
+ * Backend user records
+ */
+$tempColumnsBackend = array();
+$tempColumnsBackend['tx_authenticator_secret'] = array(
+	'exclude' => 0,
+	'label' => 'LLL:EXT:authenticator/locallang_db.xlf:be_users.tx_authenticator_secret',
+);
+if (isset($settings['showQrCodeInBackendUserRecords']) && !empty($settings['showQrCodeInBackendUserRecords'])) {
+	$tempColumnsBackend['tx_authenticator_secret']['config'] = array(
+		'type' => 'user',
+		'userFunc' => 'EXT:authenticator/Classes/Fields/QrFields.php:Tx\Authenticator\Fields\QrFields->getField'
+	);
+} else {
+	$tempColumnsBackend['tx_authenticator_secret']['config'] = array(
+		'type' => 'passthrough',
+	);
+}
+$tempColumnsBackend['tx_authenticator_enabled'] = array(
+	'label' => 'LLL:EXT:authenticator/locallang_db.xlf:be_users.tx_authenticator_enabled',
+	'config' => array(
+		'type' => 'check',
+		'items' => array(
+			array('LLL:EXT:authenticator/locallang_db.xlf:be_users.tx_authenticator_enabled_item', 0)
+		),
+		'default' => 0
+	)
 );
 
 \TYPO3\CMS\Core\Utility\GeneralUtility::loadTCA('be_users');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('be_users', $tempColumns, 1);
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('be_users', $tempColumnsBackend, TRUE);
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('be_users', 'tx_authenticator_secret;;;;1-1-1,tx_authenticator_enabled');
 
-$tempColumns = array(
-	'tx_authenticator_secret' => array(
-		'exclude' => 0,
-		'label' => 'LLL:EXT:authenticator/locallang_db.xlf:fe_users.tx_authenticator_secret',
-		'config' => array(
-			'type' => 'user',
-			'userFunc' => 'EXT:authenticator/Classes/Fields/QrFields.php:Tx\Authenticator\Fields\QrFields->getField'
-		)
-	),
-	'tx_authenticator_enabled' => array(
-		'label' => 'LLL:EXT:authenticator/locallang_db.xlf:fe_users.tx_authenticator_enabled',
-		'config' => array(
-			'type' => 'check',
-			'items' => array(
-				array('LLL:EXT:authenticator/locallang_db.xlf:fe_users.tx_authenticator_enabled_item', 0)
-			),
-			'default' => 0
-		)
-	),
+/*****
+ * Frontend user records
+ */
+$tempColumnsFrontend = array();
+$tempColumnsFrontend['tx_authenticator_secret'] = array(
+	'exclude' => 0,
+	'label' => 'LLL:EXT:authenticator/locallang_db.xlf:fe_users.tx_authenticator_secret',
+
+);
+if (isset($settings['showQrCodeInFrontendUserRecords']) && !empty($settings['showQrCodeInFrontendUserRecords'])) {
+	$tempColumnsFrontend['tx_authenticator_secret']['config'] = array(
+		'type' => 'user',
+		'userFunc' => 'EXT:authenticator/Classes/Fields/QrFields.php:Tx\Authenticator\Fields\QrFields->getField'
+	);
+} else {
+	$tempColumnsFrontend['tx_authenticator_secret']['config'] = array(
+		'type' => 'passthrough',
+	);
+}
+$tempColumnsFrontend['tx_authenticator_enabled'] = array(
+	'label' => 'LLL:EXT:authenticator/locallang_db.xlf:fe_users.tx_authenticator_enabled',
+	'config' => array(
+		'type' => 'check',
+		'items' => array(
+			array('LLL:EXT:authenticator/locallang_db.xlf:fe_users.tx_authenticator_enabled_item', 0)
+		),
+		'default' => 0
+	)
 );
 
 \TYPO3\CMS\Core\Utility\GeneralUtility::loadTCA('fe_users');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('fe_users', $tempColumns, 1);
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('fe_users', $tempColumnsFrontend, 1);
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('fe_users', 'tx_authenticator_secret;;;;1-1-1,tx_authenticator_enabled');
 
 
