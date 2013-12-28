053264a5beaaa60d37bdff500a7eb76ebbd109bb
Philipp Gampe <philipp.gampe@typo3.org>
2013-12-28 12:02:29 +0100

    [TASK] Refactor the UserAuthHook
    * Add protected to internal methods
    * Extract logic into several functions
    * Streamline functions
    * Add/Correct phpdoc
    * Introduce class variable for the user object

--- Classes/Hooks/UserAuthHook.php
+++ Classes/Hooks/UserAuthHook.php
@@ -3,9 +3,6 @@ namespace Tx\Authenticator\Hooks;
 
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Core\Authentication\AbstractUserAuthentication;
-
-require_once(ExtensionManagementUtility::extPath('authenticator') . 'Resources/Private/Php/phpqrcode/qrlib.php');
 
 /**
  * Class UserAuthHook
@@ -14,61 +11,87 @@ require_once(ExtensionManagementUtility::extPath('authenticator') . 'Resources/P
  */
 class UserAuthHook {
 
+	/** @var null|\TYPO3\CMS\Core\Authentication\AbstractUserAuthentication */
+	protected $user = NULL;
+
 	/**
-	 * Fills in the user data and checks the secret on the way
+	 * Check if authentication is needed and validate the secret
 	 *
 	 * @param $params
 	 * @param $caller
 	 */
-	function postUserLookUp(&$params, &$caller) {
-		if (TYPO3_MODE == 'BE') {
-			$user = $GLOBALS['BE_USER'];
-		} elseif (TYPO3_MODE == 'FE') {
-			$user = $GLOBALS['FE_USER'];
-		} else {
+	public function postUserLookUp(&$params, &$caller) {
+		$this->injectUser();
+		if ($this->user === NULL) {
 			// Unsupported mode, return early
 			return;
 		}
-		if ($user instanceof AbstractUserAuthentication) {
-			if ($user->user['uid']) {
-				// Ignore two factor, if two factor authentication is disabled for this user
-				if ($user->user['tx_authenticator_enabled'] & 1) {
-					// Ignore two factor authentication, if the user has no secret yet
-					if (trim($user->user['tx_authenticator_secret']) !== '') {
-						// Check whether secret was checked in session before
-						if (!$this->isValidTwoFactorInSession($user)) {
-							/** @var \Tx\Authenticator\Auth\TokenAuthenticator $authenticator */
-							$authenticator = GeneralUtility::makeInstance('Tx\\Authenticator\\Auth\\TokenAuthenticator');
-							$postTokenCheck = $authenticator->verify($user->user['username'], GeneralUtility::_GP('oneTimeSecret'));
-							if ($postTokenCheck) {
-								$this->setValidTwoFactorInSession($user);
-							} else {
-								$this->showForm(GeneralUtility::_GP('oneTimeSecret'));
-							}
-						}
-					} else {
-						/** @var \Tx\Authenticator\Auth\TokenAuthenticator $authenticator */
-						$authenticator = GeneralUtility::makeInstance('Tx\\Authenticator\\Auth\\TokenAuthenticator');
-						$authenticator->setUser($user->user['username'], 'TOTP');
-					}
-				}
+		if ($this->canAuthenticate() && $this->needsAuthentication()) {
+			/** @var \Tx\Authenticator\Auth\TokenAuthenticator $authenticator */
+			$authenticator = GeneralUtility::makeInstance('Tx\\Authenticator\\Auth\\TokenAuthenticator');
+			$postTokenCheck = $authenticator->verify($this->user->user['username'], GeneralUtility::_GP('oneTimeSecret'));
+			if ($postTokenCheck) {
+				$this->setValidTwoFactorInSession();
+			} else {
+				$this->showForm(GeneralUtility::_GP('oneTimeSecret'));
 			}
 		}
 	}
 
 	/**
+	 * Inject the user object depending on the current context
+	 *
 	 * @param \TYPO3\CMS\Core\Authentication\AbstractUserAuthentication $user
-	 * @return boolean TRUE if the user is already authenticated
+	 * @return void
 	 */
-	function isValidTwoFactorInSession($user) {
-		return $user->getSessionData('authenticatorIsValidTwoFactor') === TRUE;
+	protected function injectUser($user = NULL) {
+		if ($this->user !== NULL) {
+			// user is already injected, return early
+			return;
+		}
+		if ($user !== NULL) {
+			$this->user = $user;
+		} elseif (TYPO3_MODE == 'BE') {
+			$this->user = $GLOBALS['BE_USER'];
+		} elseif (TYPO3_MODE == 'FE') {
+			$this->user = $GLOBALS['FE_USER'];
+		}
+		if (!$this->user instanceof \TYPO3\CMS\Core\Authentication\AbstractUserAuthentication) {
+			// Invalid object or unsupported mode
+			$this->user = NULL;
+		}
 	}
 
 	/**
-	 * @param \TYPO3\CMS\Core\Authentication\AbstractUserAuthentication $user
+	 * Check for a valid user, enabled two factor authentication and if a secret is set
+	 *
+	 * @return bool
 	 */
-	function setValidTwoFactorInSession($user) {
-		$user->setAndSaveSessionData('authenticatorIsValidTwoFactor', TRUE);
+	protected function canAuthenticate() {
+		if ($this->user->user['uid'] > 0
+			&& $this->user->user['tx_authenticator_enabled'] & 1
+			&& $this->user->user['tx_authenticator_secret'] !== ''
+		) {
+			return TRUE;
+		} else {
+			return FALSE;
+		}
+	}
+
+	/**
+	 * Check whether the user is already authenticated
+	 *
+	 * @return boolean FALSE if the user is already authenticated
+	 */
+	protected function needsAuthentication() {
+		return $this->user->getSessionData('authenticatorIsValidTwoFactor') !== TRUE;
+	}
+
+	/**
+	 * Mark the current session as checked
+	 */
+	protected function setValidTwoFactorInSession() {
+		$this->user->setAndSaveSessionData('authenticatorIsValidTwoFactor', TRUE);
 	}
 
 	/**
@@ -76,7 +99,7 @@ class UserAuthHook {
 	 *
 	 * @param string $token Provided (wrong) token
 	 */
-	function showForm($token) {
+	protected function showForm($token) {
 		$error = ($token != '');
 
 		/** @var \TYPO3\CMS\Fluid\View\StandaloneView $view */
@@ -87,6 +110,15 @@ class UserAuthHook {
 		$view->assign('error', $error);
 		echo $view->render();
 		die();
+	}
+
+	/**
+	 * Create a new secret
+	 */
+	protected function createToken() {
+		/** @var \Tx\Authenticator\Auth\TokenAuthenticator $authenticator */
+		$authenticator = GeneralUtility::makeInstance('Tx\\Authenticator\\Auth\\TokenAuthenticator');
+		$authenticator->setUser($this->user->user['username'], 'TOTP');
 	}
 
 }
