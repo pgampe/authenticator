8fdb3e9f6881dd3f9be48e5c40ced0532a40bd96
Philipp Gampe <philipp.gampe@typo3.org>
2015-10-16 00:29:27 +0200

    [BUGFIX] Correct TYPO3 dependency

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '0.0.5',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.0-6.7.5',
+			'typo3' => '6.2.0-7.5.0',
 		),
 		'conflicts' => array(
 		),
