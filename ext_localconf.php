1e58c9bc4a2ca3b238062f6d8c6c2ab2de252f7c
Jonathan Starck <info@cretection.eu>
2015-11-12 23:34:04 +0100

    [FEATURE] Clean Up

--- ext_localconf.php
+++ ext_localconf.php
@@ -29,4 +29,3 @@
 
 // Register hook for user auth, use post user lookup as next possible hook AFTER user authentication
 $TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_userauth.php']['postUserLookUp'][] = 'Tx\Authenticator\Hooks\UserAuthHook->postUserLookUp';
-
