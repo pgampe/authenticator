6585220d17d3e4490239e71b73ac83905cdcc258
Jonathan Starck <info@cretection.eu>
2015-11-12 19:59:55 +0100

    Fix

--- ext_localconf.php
+++ ext_localconf.php
@@ -31,6 +31,7 @@
 $TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_userauth.php']['postUserLookUp'][] = 'Tx\Authenticator\Hooks\UserAuthHook->postUserLookUp';
 
 $extConf = unserialize($TYPO3_CONF_VARS['EXT']['extConf'][$_EXTKEY]);
+
 if (isset($extConf['showBackendLoginWithField']) && (bool)$extConf['showBackendLoginWithField']) {
-	$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['backend']['loginProviders'][1433416747]['provider'] = 'Tx\\Authenticator\\LoginProvider\\AuthenticatorLoginProvider';
+	//$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['backend']['loginProviders'][1433416747]['provider'] = 'Tx\\Authenticator\\LoginProvider\\AuthenticatorLoginProvider';
 }
