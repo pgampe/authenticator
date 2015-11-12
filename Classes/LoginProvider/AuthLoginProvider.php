1f049cb4ea3b8454af59f22e3689d414a814911d
Jonathan Starck <info@cretection.eu>
2015-11-12 20:59:55 +0100

    Test

--- Classes/LoginProvider/AuthLoginProvider.php
+++ Classes/LoginProvider/AuthLoginProvider.php
@@ -1,16 +1,17 @@
 <?php
 namespace Tx\Authenticator\LoginProvider;
 
-//use TYPO3\CMS\Backend\Controller\LoginController;
-//use TYPO3\CMS\Backend\LoginProvider\UsernamePasswordLoginProvider;
-//use TYPO3\CMS\Core\Page\PageRenderer;
-//use TYPO3\CMS\Core\Utility\GeneralUtility;
-//use TYPO3\CMS\Fluid\View\StandaloneView;
+use TYPO3\CMS\Backend\Controller\LoginController;
+use TYPO3\CMS\Backend\LoginProvider\UsernamePasswordLoginProvider;
+use TYPO3\CMS\Core\Page\PageRenderer;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Fluid\View\StandaloneView;
 
 /**
- * Class AuthenticatorLoginProvider
+ * Class AuthLoginProvider
  */
-class AuthLoginProvider extends UsernamePasswordLoginProvider {
+ 
+class AuthLoginProvider extends \TYPO3\CMS\Backend\LoginProvider\UsernamePasswordLoginProvider {
 	/**
 	 * Renders the login fields
 	 *
