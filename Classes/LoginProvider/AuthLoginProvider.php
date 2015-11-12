002289398af47f608fb5cf75a65d4f987956c1b4
Jonathan Starck <info@cretection.eu>
2015-11-12 20:54:18 +0100

    Weiter

--- Classes/LoginProvider/AuthLoginProvider.php
+++ Classes/LoginProvider/AuthLoginProvider.php
@@ -1,19 +1,6 @@
 <?php
 namespace Tx\Authenticator\LoginProvider;
 
-/*
- * This file is part of the TYPO3 CMS project.
- *
- * It is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License, either version 2
- * of the License, or any later version.
- *
- * For the full copyright and license information, please read the
- * LICENSE.txt file that was distributed with this source code.
- *
- * The TYPO3 project - inspiring people to share!
- */
-
 use TYPO3\CMS\Backend\Controller\LoginController;
 use TYPO3\CMS\Backend\LoginProvider\UsernamePasswordLoginProvider;
 use TYPO3\CMS\Core\Page\PageRenderer;
@@ -23,8 +10,7 @@ use TYPO3\CMS\Fluid\View\StandaloneView;
 /**
  * Class AuthenticatorLoginProvider
  */
-class AuthenticatorLoginProvider extends UsernamePasswordLoginProvider {
-
+class AuthLoginProvider extends UsernamePasswordLoginProvider {
 	/**
 	 * Renders the login fields
 	 *
@@ -36,5 +22,4 @@ class AuthenticatorLoginProvider extends UsernamePasswordLoginProvider {
 		parent::render($view, $pageRenderer, $loginController);
 		$view->setTemplatePathAndFilename(GeneralUtility::getFileAbsFileName('EXT:authenticator/Resources/Private/Templates/LoginAuthenticator.html'));
 	}
-
 }
