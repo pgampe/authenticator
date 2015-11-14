8e59937d68fe1d13dd3d5ca7ce637b45bc41085c
Philipp Gampe <philipp.gampe@typo3.org>
2015-11-14 12:57:06 +0100

    [TASK] Move TCA modifications to Configuration/TCA/Overrides/*.php

---
+++ Configuration/TCA/Overrides/be_users.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+$tempColumnsBackend = array();
+$tempColumnsBackend['tx_authenticator_secret'] = array(
+    'exclude' => 0,
+    'label' => 'LLL:EXT:authenticator/Resources/Private/Language/locallang_db.xlf:be_users.tx_authenticator_secret',
+);
+$tempColumnsBackend['tx_authenticator_secret']['config'] = array(
+    'type' => 'passthrough',
+);
+$tempColumnsBackend['tx_authenticator_enabled'] = array(
+    'label' => 'LLL:EXT:authenticator/Resources/Private/Language/locallang_db.xlf:be_users.tx_authenticator_enabled',
+    'config' => array(
+        'type' => 'check',
+        'items' => array(
+            array('LLL:EXT:authenticator/Resources/Private/Language/locallang_db.xlf:be_users.tx_authenticator_enabled_item', 0)
+        ),
+        'default' => 0
+    )
+);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('be_users', $tempColumnsBackend);
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('be_users', 'tx_authenticator_enabled');
---
+++ Configuration/TCA/Overrides/fe_users.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+$tempColumnsFrontend = array();
+$tempColumnsFrontend['tx_authenticator_secret'] = array(
+    'exclude' => 0,
+    'label' => 'LLL:EXT:authenticator/Resources/Private/Language/locallang_db.xlf:fe_users.tx_authenticator_secret',
+);
+$tempColumnsFrontend['tx_authenticator_secret']['config'] = array(
+    'type' => 'passthrough',
+);
+$tempColumnsFrontend['tx_authenticator_enabled'] = array(
+    'label' => 'LLL:EXT:authenticator/Resources/Private/Language/locallang_db.xlf:fe_users.tx_authenticator_eaddTCAcolumnsnabled',
+    'config' => array(
+        'type' => 'check',
+        'items' => array(
+            array('LLL:EXT:authenticator/Resources/Private/Language/locallang_db.xlf:fe_users.tx_authenticator_enabled_item', 0)
+        ),
+        'default' => 0
+    )
+);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('fe_users', $tempColumnsFrontend);
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('fe_users', 'tx_authenticator_enabled');
