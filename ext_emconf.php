1147be85ad92acd6e00ef7c47ff86968ccf7e4bf
Oliver Eglseder <oliver.eglseder@in2code.de>
2016-11-18 16:06:25 +0100

    [META] Mark as compatible with TYPO3 7.6 and 8.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '0.1.1',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '7.4.0-7.6.99',
+            'typo3' => '7.6.0-8.9.99',
         ),
         'conflicts' => array(),
         'suggests' => array(),
