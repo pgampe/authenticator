73ca9920804cceb26be3b99a41eb1cdf3d69b547
Oliver Eglseder <oliver.eglseder@in2code.de>
2016-11-18 16:20:15 +0100

    [CLEANUP] Remove deprecated/removed keys from EmConf

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,41 +1,22 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "authenticator".
-#
-# Auto generated 15-05-2012 21:40
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
-
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = [
     'title' => 'Authenticator',
     'description' => 'Implements a two factor authentication for TYPO3. Currently a for backend only.',
     'category' => 'services',
     'author' => 'Philipp Gampe',
     'author_email' => 'philipp.gampe@typo3.org',
     'author_company' => '',
-    'shy' => '',
-    'dependencies' => 'core',
-    'conflicts' => '',
-    'priority' => '',
-    'module' => '',
     'state' => 'stable',
-    'internal' => '',
     'uploadfolder' => 0,
     'createDirs' => '',
-    'modify_tables' => '',
     'clearCacheOnLoad' => 1,
-    'lockType' => '',
     'version' => '0.1.1',
-    'constraints' => array(
-        'depends' => array(
+    'constraints' => [
+        'depends' => [
             'typo3' => '7.6.0-8.9.99',
-        ),
-        'conflicts' => array(),
-        'suggests' => array(),
-    ),
-    '_md5_values_when_last_written' => '',
-);
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
