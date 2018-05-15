4473c6281ee1795fd13a8d25ef7c8605f83164a6
Oliver Eglseder <php@vxvr.de>
2018-05-15 17:02:49 +0200

    [BUGFIX] Deny direct access to extr_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,4 +1,7 @@
 <?php
+if (!defined('TYPO3_MODE')) {
+    die('Access denied.');
+}
 
 // Register hook for user auth, use post user lookup as next possible hook AFTER user authentication
 $TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_userauth.php']['postUserLookUp'][] = 'Tx\Authenticator\Hooks\UserAuthHook->postUserLookUp';
